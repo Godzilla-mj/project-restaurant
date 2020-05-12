78f02187935b3ce7c17206419cee115142580849
Myung <god.godzilla.af@gmail.com>
2020-05-11 20:04:31 -0400

    contact.js WIP, else minor changes

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -5,10 +5,12 @@ function renderContact() {
   const navBar = document.createElement('div')
   navBar.classList.add('navBar')
 
+    const logo = document.createElement('div')
     const logoImg = document.createElement('img')
     logoImg.classList.add('logo')
     logoImg.setAttribute( 'src', 'imgs/salty weenie hut.png')
-    navBar.appendChild(logoImg)
+    logo.appendChild(logoImg)
+    navBar.appendChild(logo)
 
     const home = document.createElement('div')
     home.innerHTML = "Home"
@@ -28,12 +30,33 @@ function renderContact() {
   const contactContent = document.createElement('div')
   contactContent.classList.add('content-contact')
 
-    const contactHead = document.createElement('h1')
-      contactHead.innerHTML = 'Testing contactHead'
-      contactContent.appendChild(contactHead)
-    const contactTxt = document.createElement('p')
-      contactTxt.innerHTML = 'Testing contactTxt'
-      contactContent.appendChild(contactTxt)
+    const leftSide = document.createElement('div')
+    leftSide.classList.add('ccContainer')
+    contactContent.appendChild(leftSide)
+
+      const leftSideTitle = document.createElement('h3')
+      leftSideTitle.innerHTML = "Contact Us!"
+      leftSideTitle.classList.add('cc')
+      leftSide.appendChild(leftSideTitle)
+
+      const leftSideInfo = document.createElement('p')
+      leftSideInfo.innerHTML = "124 Conch Street, Bikini Bottom, Pacific Ocean"
+      leftSideInfo.classList.add('cc')
+      leftSide.appendChild(leftSideInfo)
+
+    const rightSide = document.createElement('div')
+    rightSide.classList.add('ccContainer')
+    contactContent.appendChild(rightSide)
+
+      const rightSideTitle = document.createElement('h3')
+      rightSideTitle.innerHTML = "Sorry! We're located in the ocean!"
+      rightSideTitle.classList.add('cc')
+      rightSide.appendChild(rightSideTitle)
+
+      const rightSideInfo = document.createElement('p')
+      rightSideInfo.innerHTML = "Don't worry! We're in the process of opening a location near you~! SOON!"
+      rightSideInfo.classList.add('cc')
+      rightSide.appendChild(rightSideInfo)
 
   content.appendChild(navBar)
   content.appendChild(contactContent)
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,10 +5,12 @@ function renderMenu() {
   const navBar = document.createElement('div')
   navBar.classList.add('navBar')
 
+    const logo = document.createElement('div')
     const logoImg = document.createElement('img')
     logoImg.classList.add('logo')
     logoImg.setAttribute( 'src', 'imgs/salty weenie hut.png')
-    navBar.appendChild(logoImg)
+    logo.appendChild(logoImg)
+    navBar.appendChild(logo)
 
     const home = document.createElement('div')
     home.innerHTML = "Home"
@@ -24,6 +26,7 @@ function renderMenu() {
     contact.innerHTML = 'Contact'
     contact.classList.add('rightNavBarItem')
     navBar.appendChild(contact)
+
   const menuContent = document.createElement('div')
   menuContent.classList.add('content-menu')
 
